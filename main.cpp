8aff53fbdd147e3e7f0e1aea2ef430600bceecd4
AuroraFranceschini <115407804+AuroraFranceschini@users.noreply.github.com>
2022-12-19 17:56:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include "cmath"
 using namespace std;
 
+float f (float y) {
+    float risultato = 0;
+    risultato = pow(y,2) * cos(y) +1;
+    return risultato;
+}
+
 int main() {
-   return 0;
+    float a = 0, b = 0, x = 0, err = 0;
+    do{
+        cout << "inserire estremi" << endl;
+        cin >> a;
+        cin >> b;
+
+    } while (f(a)*f(b) >= 0);
+
+    do {
+        x = (a+b)/2;
+        if (f(x)==0){
+            cout << x << endl;
+            cout << f(x) << endl;
+            return 0;
+        } else {
+            if (f(a)*f(b) <0){
+                b = x;
+            } else {
+                a = x;
+            }
+            err = abs((b-a)/2);
+        }
+    } while (err >= exp(1) -6 );
+    cout << (x*1000)/1000.0 <<endl;
+    cout << f(x) << endl;
+
+    return 0;
 }
